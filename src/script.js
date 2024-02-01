06c7b7b8d2f0c55c9f2ed95729e5d3603321bfb0
kamyar <kamyarkarimi98@gmail.com>
2024-02-01 14:22:10 -0500

    test json

--- src/script.js
+++ src/script.js
@@ -2,4 +2,5 @@ fetch("../data.json")
 	.then((response) => response.json())
 	.then((collection) => {
 		console.log(collection);
+		document.querySelector(".test").innerHTML = collection[0].title;
 	});
